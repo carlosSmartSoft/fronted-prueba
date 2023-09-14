bd9771f0f2ddc93679f7a0270528414eef56ac14
Carlos Diaz <carlos.diaz@smartsoft.com.co>
2023-09-13 19:38:58 -0500

    add new env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://177k46gvu3.execute-api.us-east-1.amazonaws.com',
+  API_URL: 'https://gao34zo6m4.execute-api.us-east-1.amazonaws.com',
 };
